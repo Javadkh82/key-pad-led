ad74d73c360b56948db5427cc89ded5547c93645
javad_KH <166110182+Javadkh82@users.noreply.github.com>
2024-05-02 18:53:01 +0330

    key pad & led code.ino

---
+++ key pad & led code.ino
@@ -0,0 +1,29 @@
+
+#include <Keypad.h>
+int LED = 10;
+const byte ROWS = 4; 
+const byte COLS = 4; 
+
+char hexaKeys[ROWS][COLS] = {
+ {'1', '2', '3', 'A'},
+ {'4', '5', '6', 'B'},
+ {'7', '8', '9', 'C'},
+ {'*', '0', '#', 'D'}
+};
+byte colPins[ROWS] = {5, 4, 3, 2}; 
+byte rowPins[COLS] = {9, 8, 7, 6}; 
+
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS,COLS);
+void setup(){
+ Serial.begin(9600);
+ pinMode ( LED, OUTPUT);
+}
+void loop(){
+ char customKey = customKeypad.getKey();
+ if (customKey== 49){
+  digitalWrite ( LED, HIGH);
+ }
+ else if (customKey== 50){
+    digitalWrite ( LED, LOW);
+  }
+ }
